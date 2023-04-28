fd56b307eeed51e4674a346bb241ab6d9700220c
root <showell30@yahoo.com>
2023-04-28 07:50:06 +0000

    Avoid space bar double-toggling the pane.

--- fib.js
+++ fib.js
@@ -270,6 +270,7 @@ function set_toggle_handler() {
 function set_keyboard_handler() {
     document.addEventListener("keydown", (e) => {
         if (e.key == "t" || e.key == " ") {
+            e.preventDefault();
             toggle_mode();
             return;
         }
